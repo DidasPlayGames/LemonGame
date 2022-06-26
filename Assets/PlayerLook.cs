d01acc9bec3985e9f4b8f223442d7fd51a82aaf7
DidasPlayGames <didas161@gmail.com>
2022-06-26 17:59:57 +0100

    Finished Basic Controller
    - Downloaded ProBuilder
    - Check for ground and gravity
    - Proper movement (Not translation)

--- Assets/PlayerLook.cs
+++ Assets/PlayerLook.cs
@@ -14,11 +14,16 @@ public class PlayerLook : MonoBehaviour
     // Variables for Moving
     private float horizontalInput;
     private float verticalInput;
+    [SerializeField] private CharacterController controller;
     [SerializeField] private int movementSpeed;
 
-    //Variables for Jumping
-    [SerializeField] private Rigidbody playerRigidbody;
-    [SerializeField] private float jumpForce;
+    //Variables for Gravity and Ground-Checking
+    Vector3 gravityVelocity;
+    [SerializeField] private float gravityStength;
+    [SerializeField] private GameObject groundCheck;
+    [SerializeField] private float groundDistance = 0.4f;
+    [SerializeField] private LayerMask groundMask;
+    private bool isGrounded;
 
     void Start()
     {   
@@ -29,31 +34,46 @@ public class PlayerLook : MonoBehaviour
     void Update()
     {   
         //Calls all processes
-        Look();
         Move();
+        Look();
+        ApplyGravity();
+    }
 
+    void ApplyGravity(){
+        isGrounded = Physics.CheckSphere(groundCheck.transform.position, groundDistance, groundMask);
+
+        if(isGrounded && gravityVelocity.y < 0){
+            gravityVelocity.y = -2f;
+        }
+
+        //Creates a Vector3, which will be used to apply the force of gravity
+        gravityVelocity.y += gravityStength * Time.deltaTime;
+        //Applies the gravityVelocity Vector3, using the CharacterController
+        controller.Move(gravityVelocity * Time.deltaTime);
     }
 
     //Handles translating the player with input from the keyboard
     void Move(){
+        //Gathers input from the keyboard and converts into appropriate value
         horizontalInput = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
         verticalInput = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
 
-        Debug.Log(verticalInput);
-
-        transform.Translate(Vector3.forward * verticalInput);
-        transform.Translate(Vector3.right * horizontalInput);
+        //Moves using CharacterController, according to player's input
+        Vector3 move = transform.right * horizontalInput + transform.forward * verticalInput;
+        controller.Move(move);
     }
 
     //Handles looking around and input with the mouse
     void Look(){
-        
+        //Gathers input from the mouse and converts into approriate value
         mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        //Handles generating an appropriate angle for vertical rotation
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90, 90);
 
+        //Rotates the player and camera according to player's input
         playerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
         transform.Rotate(Vector3.up * mouseX);
     }
